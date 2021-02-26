3e63944f2367ad3f5c511223b45634a99f707e5e
Patrick Muehlbauer <patrick@opentrash.org>
2021-02-26 10:45:07 +0100

    optionally configure production endpoint via envvars

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,10 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :battleship, BattleshipWeb.Endpoint,
-  url: [host: "battleship.gigalixirapp.com", port: 443],
+  url: [
+    host: System.get_env("HOST", "battleship.gigalixirapp.com"),
+    port: System.get_env("PORT", "443") |> String.to_integer()
+  ],
   cache_static_manifest: "priv/static/cache_manifest.json",
   force_ssl: [rewrite_on: [:x_forwarded_proto]]
 
